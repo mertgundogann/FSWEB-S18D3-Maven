969fbb70a20bbbf02e83fa369fbb52c16ee1da25
mertgundogann <merttgundogan@hotmail.com>
2025-04-21 23:38:47 +0300

    push

--- src/main/java/com/workintech/sqlrelations/repository/DoctorRepository.java
+++ src/main/java/com/workintech/sqlrelations/repository/DoctorRepository.java
@@ -3,5 +3,6 @@ package com.workintech.sqlrelations.repository;
 import com.workintech.sqlrelations.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 }
